c44bdec33d4668f321858e2bf8ff22e297fea30f
10Hyuna <kimhaa1005@naver.com>
2025-05-14 15:13:01 +0900

    init project

---
+++ src/main/java/com/example/opensource/MovieReviewersApplication.java
@@ -0,0 +1,13 @@
+package com.example.opensource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReviewersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReviewersApplication.class, args);
+	}
+
+}
